a4597bde632780e96f1c9b3777f479502f5c43d6
Andreas Sauer <18749753+Hanibal1963@users.noreply.github.com>
2024-01-28 12:24:37 +0100

    Codebereinigung durchgeführt.

--- DriveWatcherControl/DriveWatcher.vb
+++ DriveWatcherControl/DriveWatcher.vb
@@ -1,7 +1,7 @@
 ﻿'
 '****************************************************************************************************************
 'DriveWatcher.vb
-'(c) 2023 by Andreas Sauer
+'© 2023 - 2024 by Andreas Sauer
 '****************************************************************************************************************
 '
 
@@ -18,57 +18,28 @@ Public Class DriveWatcher
 
     Private WithEvents _Form As New NativeForm
 
-    ''' <summary>
-    ''' Wird ausgelöst wenn ein Laufwerk hinzugefügt wurde.
-    ''' </summary>
+    ''' <summary>Wird ausgelöst wenn ein Laufwerk hinzugefügt wurde.</summary>
     ''' <param name="sender"></param>
-    ''' <param name="e">
-    ''' Enthält die Eigenschaften zum hinzugefügten Laufwerk. (<see cref="DriveAddedEventArgs"/>)
-    ''' </param>
+    ''' <param name="e">Enthält die Eigenschaften zum hinzugefügten Laufwerk. (<see cref="DriveAddedEventArgs"/>)</param>
     Public Event DriveAdded(sender As Object, e As DriveAddedEventArgs)
 
-    ''' <summary>
-    ''' Wird ausgelöst wenn ein Laufwerk entfernt wurde.
-    ''' </summary>
+    ''' <summary>Wird ausgelöst wenn ein Laufwerk entfernt wurde.</summary>
     ''' <param name="sender"></param>
-    ''' <param name="e">
-    ''' Enthält die eigenschaften zum entfernten Laufwerk. (<see cref="DriveRemovedEventArgs"/>)
-    ''' </param>
+    ''' <param name="e">Enthält die eigenschaften zum entfernten Laufwerk. (<see cref="DriveRemovedEventArgs"/>)</param>
     Public Event DriveRemoved(sender As Object, e As DriveRemovedEventArgs)
 
 
-    ''' <summary>
-    ''' wird ausgeführt wenn ein Laufwerk hinzugefügt wurde.
-    ''' </summary>
-    ''' <param name="sender"></param>
-    ''' <param name="e">
-    ''' Enthält Informationen über das Laufwerk.
-    ''' </param>
     Private Sub _Form_DriveAdded(sender As Object, e As System.IO.DriveInfo) Handles _Form.DriveAdded
-
         Dim arg As New DriveAddedEventArgs With {
             .DriveName = e.Name, .VolumeLabel = e.VolumeLabel, .AvailableFreeSpace = e.AvailableFreeSpace,
             .TotalFreeSpace = e.TotalFreeSpace, .TotalSize = e.TotalSize, .DriveFormat = e.DriveFormat,
             .DriveType = e.DriveType, .IsReady = e.IsReady}
-
         RaiseEvent DriveAdded(Me, arg)
-
     End Sub
 
-
-    ''' <summary>
-    ''' wird ausgeführt wenn ein Laufwerk entfernt wurde.
-    ''' </summary>
-    ''' <param name="sender"></param>
-    ''' <param name="e">
-    ''' Enthält Informationen über das Laufwerk.
-    ''' </param>
     Private Sub _Form_DriveRemoved(sender As Object, e As System.IO.DriveInfo) Handles _Form.DriveRemoved
-
         Dim arg As New DriveRemovedEventArgs With {.DriveName = e.Name}
-
         RaiseEvent DriveRemoved(Me, arg)
-
     End Sub
 
 End Class
--- DriveWatcherControl/NativeForm.vb
+++ DriveWatcherControl/NativeForm.vb
@@ -8,49 +8,35 @@
 Imports System.Windows.Forms
 Imports System.IO
 
-
 Friend Class NativeForm
 
 	Inherits NativeWindow
-
 
 	'Das sind die Ereignisse aus WParam.
 	'Uns interessiert nur, ob ein Laufwerk hinzugekommen ist oder entfernt wurde.
 	Public Event DriveAdded(sender As Object, e As DriveInfo)
 	Public Event DriveRemoved(sender As Object, e As DriveInfo)
 
-
 	'Windowmessage DeviceChange
 	Private Const WM_DEVICECHANGE As Integer = &H219
-
 
 	'Die beiden Ereignisse, die für uns von Bedeutung sind.
 	Private Const DBT_DEVICEARRIVAL As Integer = &H8000
 	Private Const DBT_DEVICEREMOVECOMPLETE As Integer = &H8004
 
-
 	'Dies ist der Dreh- und Angelpunkt der Klasse. - Hier bekommen wir die Messages mit.
 	'In unserm Fall interessiert uns nur die WM_DeviceChange-Nachricht
 	Protected Overrides Sub WndProc(ByRef m As Message)
-
-		If m.Msg = WM_DEVICECHANGE Then
-			Me.HandleHeader(m)
-		End If
+		If m.Msg = WM_DEVICECHANGE Then Me.HandleHeader(m)
 		MyBase.WndProc(m)
-
 	End Sub
-
 
 	'Hier schauen wir erst mal in den Header und verzweigen dementsprechend
 	Private Sub HandleHeader(ByRef m As Message)
-
 		Dim header As DEV_BROADCAST_HDR
 		Dim objHeader As Object = m.GetLParam(header.GetType)
-
 		If Not Microsoft.VisualBasic.IsNothing(objHeader) Then
-
 			Select Case header.dbch_devicetype
-
 				Case DBT_DEVTYP.OEM : Me.HandleOEM(m)
 				Case DBT_DEVTYP.DEVNODE
 				Case DBT_DEVTYP.VOLUME : Me.HandleVolume(m)
@@ -58,95 +44,55 @@ Friend Class NativeForm
 				Case DBT_DEVTYP.NET
 				Case DBT_DEVTYP.DEVICEINTERFACE
 				Case DBT_DEVTYP.HANDLE
-
 			End Select
-
 		End If
-
 	End Sub
-
 
 	'Das Ereignis betrifft ein Volume
 	Private Sub HandleVolume(ByRef m As Message)
-
 		Dim volume As DEV_BROADCAST_VOLUME
 		Dim objVolume As Object = m.GetLParam(volume.GetType)
-
 		If Not Microsoft.VisualBasic.IsNothing(objVolume) Then
-
 			volume = DirectCast(objVolume, DEV_BROADCAST_VOLUME)
 			Dim di As New DriveInfo(Me.DriveFromMask(volume.dbcv_unitmask))
-
 			Select Case CInt(m.WParam)
-
 				Case DBT_DEVICEARRIVAL : RaiseEvent DriveAdded(Me, di)
 				Case DBT_DEVICEREMOVECOMPLETE : RaiseEvent DriveRemoved(Me, di)
-
 			End Select
-
-
-
-
 		End If
-
 	End Sub
-
 
 	'OEM, und was genau?
 	'Uns interesieren nur Volumes
 	Private Sub HandleOEM(ByRef m As Message)
-
 		Dim oem As DEV_BROADCAST_OEM
 		Dim objOem As Object = m.GetLParam(oem.GetType)
-
 		If Not Microsoft.VisualBasic.IsNothing(objOem) Then
-
 			oem = DirectCast(objOem, DEV_BROADCAST_OEM)
-
-			If oem.dbco_devicetype = DBT_DEVTYP.VOLUME Then
-				Me.HandleVolume(m)
-			End If
-
+			If oem.dbco_devicetype = DBT_DEVTYP.VOLUME Then Me.HandleVolume(m)
 		End If
-
 	End Sub
-
 
 	'Liefert den Laufwerksbuchstaben zurück
 	Private Function DriveFromMask(mask As Integer) As Char
-
 		Dim result As Char = CChar(String.Empty)
-
 		For b As Integer = 0 To 25
-
 			If (mask And CInt(2 ^ b)) <> 0 Then
 				result = Microsoft.VisualBasic.Chr(65 + b)
 				Exit For
 			End If
-
 		Next b
-
 		Return result
-
 	End Function
 
-
 	Public Sub New()
-
-		'eigenes Handle erstellen
-		Me.CreateHandle(New CreateParams)
-
+		Me.CreateHandle(New CreateParams) 'eigenes Handle erstellen
 	End Sub
-
 
 	Protected Overrides Sub Finalize()
-
-		'eigenes Handle zerstören
-		Me.DestroyHandle()
+		Me.DestroyHandle() 'eigenes Handle zerstören
 		MyBase.Finalize()
-
 	End Sub
-
 
 End Class
 

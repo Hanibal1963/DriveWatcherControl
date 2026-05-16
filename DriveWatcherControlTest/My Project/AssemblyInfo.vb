0bfb9356962d80556397fd01554fdff5f3fa081d
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2026-05-16 11:57:49 +0200

    Projektdateien hinzufügen.

---
+++ DriveWatcherControlTest/My Project/AssemblyInfo.vb
@@ -0,0 +1,16 @@
+' --------------------------------------------------------------------------------------------------------
+' Datei: AssemblyInfo.vb
+' Author: Andreas Sauer
+' Datum: 30.04.2026
+' --------------------------------------------------------------------------------------------------------
+
+<Assembly: Reflection.AssemblyTitle("DriveWatcherControlTest")>
+<Assembly: Reflection.AssemblyDescription("Testprogramm für DriveWatcher Control")>
+<Assembly: Reflection.AssemblyCompany("privat")>
+<Assembly: Reflection.AssemblyProduct("DriveWatcherControlTest")>
+<Assembly: Reflection.AssemblyCopyright("Copyright © 2026 Andreas Sauer")>
+<Assembly: Reflection.AssemblyTrademark("SchlumpfSoft")>
+<Assembly: Runtime.InteropServices.ComVisible(True)>
+<Assembly: Runtime.InteropServices.Guid("dae7ef98-2e0d-470f-972c-d2c5cdfb3d20")>
+<Assembly: Reflection.AssemblyVersion("1.2026.0505.8")>
+<Assembly: Reflection.AssemblyFileVersion("1.2026.0505.8")>

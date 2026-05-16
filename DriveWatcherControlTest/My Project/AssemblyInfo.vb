06554ae74cfb4aea2cb5788d562a35469e474557
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2026-05-16 12:08:29 +0200

    Aktualisiere Assembly-Versionen in beiden Projekten
    Die Assembly-Versionen und Datei-Versionsnummern wurden in den Projekten `DriveWatcherControl` und `DriveWatcherControlTest` aktualisiert:
    
    - `DriveWatcherControl`: Version von `1.2026.0505.25` auf `1.2026.0505.26`
    - `DriveWatcherControlTest`: Version von `1.2026.0505.8` auf `1.2026.0505.9`

--- DriveWatcherControlTest/My Project/AssemblyInfo.vb
+++ DriveWatcherControlTest/My Project/AssemblyInfo.vb
@@ -12,5 +12,5 @@
 <Assembly: Reflection.AssemblyTrademark("SchlumpfSoft")>
 <Assembly: Runtime.InteropServices.ComVisible(True)>
 <Assembly: Runtime.InteropServices.Guid("dae7ef98-2e0d-470f-972c-d2c5cdfb3d20")>
-<Assembly: Reflection.AssemblyVersion("1.2026.0505.8")>
-<Assembly: Reflection.AssemblyFileVersion("1.2026.0505.8")>
+<Assembly: Reflection.AssemblyVersion("1.2026.0505.9")>
+<Assembly: Reflection.AssemblyFileVersion("1.2026.0505.9")>
